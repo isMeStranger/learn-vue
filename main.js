a0aed4906c4a05609d537dd826ea788c6d929014
dev global <salarAdelSabry@gmail.com>
2023-07-26 22:59:47 +0300

    lesson-1 finished

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product: 'Socks',
+            description: 'This is the description for Socks product.'
+        }
+    }
+});
+
+const mountedApp = app.mount('#app');
