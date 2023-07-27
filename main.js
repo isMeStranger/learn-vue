b65f58ae3c181e4552c137ce5bd62ce52fee7337
dev global <salarAdelSabry@gmail.com>
2023-07-27 10:49:34 +0300

    lesson 3 end

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            image:'./assets/images/socks_green.jpg',
-            url:'#'
+            image: './assets/images/socks_blue.jpg',
+            inStock: true,
+            inventory: 5
         }
     }
 })
