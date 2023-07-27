3e1b298b206783c791e58ec26f8c5850ae4e8655
dev global <salarAdelSabry@gmail.com>
2023-07-27 10:58:52 +0300

    Lesson 4 end

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# learn-vue"
+Rendering Lists 
+
+the :key attribute needs a unique id so that vue can keep track of items in a list
--- main.js
+++ main.js
@@ -4,7 +4,11 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            inventory: 5
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:1, color:'green'},
+                {id:2, color:'blue'}
+            ]            
         }
     }
 })
