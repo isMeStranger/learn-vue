a6ae35f451a0ba29dd5056aaed36a8610e9f3b6f
dev global <salarAdelSabry@gmail.com>
2023-07-27 12:34:34 +0300

    first commit

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            product: 'Socks',
-            brand: 'Vue Mastery',            
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:15 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', qunatity:0 },
-            ]
+            cart: 0,
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        }
-    },
-    computed:{
-        // cache and boost performance
-        title(){            
-            return this.brand + ' ' + this.product;
-        },
-        image(){
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock(){
-            return this.variants[this.selectedVariant].quantity;
-        }
+
     }
 })
